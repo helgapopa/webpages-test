927467395ae4cc61ad55361d09e2312657ae157d
Ana Popa <ana_maria.popaa@yahoo.com>
2020-03-04 13:18:26 +0200

    test websites
    -work in progress
    -test of websites

---
+++ b2w/assets/js/main.js
@@ -0,0 +1,36 @@
+$(function() {
+
+	// Cache the windows object
+	var $window = $(window);
+
+	// Parallax background effect
+	$('section[data-type="background"]').each(function)) {
+		var $bgobj = $(this); // assigning the object
+
+		$(window).scroll(function)) {
+		
+			// scroll the background at var speed
+			// the yPos is a negative value because we're scroling it UP!
+
+			var yPos = -($window.scrollTop)) / $bgobj.data('speed'));
+		
+			// Put together our final background position
+			var coords = '50% '+ Ypos + 'px';
+
+			// Move the background
+			$bgobj.css({ backgroundPosition: coords});
+
+			// end window scroll
+
+		});
+
+	});
+
+});
+
+
+
+
+
+
+
